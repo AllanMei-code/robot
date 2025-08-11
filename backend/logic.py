440e6786cd6ef9c6b6fee8b444b3cefdcfcfc204
Allan <allanbluesky@gmail.com>
2025-08-11 11:58:11 +0800

    modified:   backend/logic.py

--- backend/logic.py
+++ backend/logic.py
@@ -3,11 +3,12 @@
 def get_bot_reply(message: str) -> str:
     message = message.lower()
 
-    if "你好" in message:
-        return "你好！有什么可以帮您？"
-    elif "退款" in message:
-        return "关于退款，请提供订单号，我们将尽快为您处理。"
-    elif "营业时间" in message:
-        return "我们的营业时间是每天9:00 - 18:00。"
+    if "Bonjour" in message:
+        return "Bonjour, bienvenue chez gamesawa，Comment puis-je vous aider ?"
+    elif "*" in message:
+        return "Veuillez décrire en détail le problème que vous avez rencontré"
+    elif "J'ai effectué un retrait que je n'ai pas encore reçu" in message :
+        return "En raison de l'instabilité des canaux de paiement, veuillez patienter" 
     else:
         return "抱歉，我暂时无法回答此问题，我们的人工客服会尽快联系您。"
+     
